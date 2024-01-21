fc6d3b7b887df95d42a6d54b994d609e9a4252c3
irfan-za <fanzahran123@gmail.com>
2024-01-21 18:07:34 +0700

    fix: update footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className='pt-16 flex'>
-      <span className="py-4 bg-blue-600 text-white text-center w-full">© {new Date().getFullYear()} Synapsis.id - situs blog sederhana.</span>
+      <span className="py-4 bg-blue-600 text-white text-center w-full">© {new Date().getFullYear()} Synapsis.id - All rights reserved.</span>
     </div>
   )
 }
